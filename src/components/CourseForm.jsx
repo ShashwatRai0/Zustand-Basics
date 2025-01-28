5108154dea2d276dcf3b2a9c9a377865ddc157b0
Shashwat Rai <shashwatrai54@gmail.com>
2025-01-28 12:47:03 +0530

    Initial commit: add zustand store

---
+++ src/components/CourseForm.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+import useCourseStore from '../app/courseStore'
+
+
+const CourseForm = () => {
+  const addCourse = useCourseStore((state) => state.addCourse)
+  const [courseTitle, setCourseTitle] = useState('')
+
+  const handleCourseSubmit = () => {
+    if (!courseTitle) return alert('Please add a course title')
+    const newCourse = {
+      id: Math.ceil(Math.random() * 10000),
+      title: courseTitle,
+      completed: false,
+    }
+    addCourse(newCourse)
+    setCourseTitle("")
+  }
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={courseTitle}
+        onChange={(e) => setCourseTitle(e.target.value)}
+      />
+      <button onClick={handleCourseSubmit}>Add Course</button>
+    </div>
+  )
+}
+
+export default CourseForm
